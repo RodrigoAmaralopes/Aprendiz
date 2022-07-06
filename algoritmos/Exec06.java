dbef44c2e12aed6b978a989d38eec9b143d2664a
Rodrigo Amaral <108424004+RodrigoAmaralopes@users.noreply.github.com>
2022-07-06 17:30:34 -0300

    Add files via upload

---
+++ algoritmos/Exec06.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.br.senac.algoritmos;
+
+import java.util.Scanner;
+
+/*
+
+6. Criar algoritmo que leia 10 numeros e identifique qual é o 
+maior e menor entre eles.
+Ao final apresenta a lista de valores e o maior valor e menor valor.
+
+
+pt 2 reordenar em ordem crescente 
+ */
+/**
+ *
+ * @author Rodrigo Amaral
+ */
+public class Exec06 {
+
+    public static void main(String[] args) {
+
+        Scanner ler = new Scanner(System.in);
+        int maiorValor
+                int menorValor;
+        int valores[] = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Entre com o " + (i + 1) + "º números: ");
+            int valores = ler.nextInt();
+            
+           //NÃO ESTÁ TERMINADO 
+                
+            }
+
+        }
+    }
+
+}
